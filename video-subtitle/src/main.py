9d84bcac94dc2d3cd0e42f01beee4253da3d00dd
m15kh <mohammad.khalili1515@gmail.com>
2025-03-04 06:14:18 +0900

    works on eng subtitle

---
+++ video-subtitle/src/main.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append("..") 
+
+from models import captacity
+
+captacity.add_captions(
+    video_file="/home/rteam2/m15kh/video-subtitle/input/fa-tst4.mp4",
+    output_file="/home/rteam2/m15kh/video-subtitle/result/fa-tst4-edit.mp4",
+    #/home/rteam2/.fonts/truetype/Vazir/vazirmatn-master/fonts/ttf/Vazirmatn-Black.ttf
+    #/home/rteam2/m15kh/video-subtitle/notebook/auto-subtitle/auto_subtitle/captacity/captacity/assets/fonts/Bangers-Regular.ttf",
+
+    font = "/home/rteam2/.fonts/truetype/Vazir/vazirmatn-master/fonts/ttf/Vazirmatn-Black.ttf",
+    font_size = 130,
+    font_color = "white",
+
+    stroke_width = 2,
+    stroke_color = "black",
+
+    shadow_strength = 1.0,
+    shadow_blur = 0.8,
+
+    highlight_current_word = True,
+    word_highlight_color = "blue",
+
+    line_count=2,
+
+    padding = 50,
+    print_info = True,
+    
+    
+)
---
+++ video-subtitle/src/test-subtitle.py
@@ -0,0 +1,24 @@
+from moviepy import VideoFileClip, TextClip, CompositeVideoClip
+
+# Load file example.mp4 and keep only the subclip from 00:00:10 to 00:00:20
+# Reduce the audio volume to 80% of its original volume
+
+clip = (
+    VideoFileClip("/home/rteam2/m15kh/video-subtitle/notebook/auto-subtitle/auto_subtitle/input/test1.mp4")
+    .subclipped(10, 20)
+    .with_volume_scaled(0.8)
+)
+
+# Generate a text clip. You can customize the font, color, etc.
+txt_clip = TextClip( #/home/rteam2/.fonts/truetype/Vazir/vazirmatn-master/fonts/ttf/Vazirmatn-Black.ttf
+    #"/home/rteam2/m15kh/video-subtitle/notebook/auto-subtitle/auto_subtitle/captacity/captacity/assets/fonts/Bangers-Regular.ttf",
+ 
+    font="/home/rteam2/.fonts/truetype/Vazir/vazirmatn-master/fonts/ttf/Vazirmatn-Black.ttf",
+    text=" سید چه خبر سلام خوبی",
+    font_size=70,
+    color='white'
+).with_duration(10).with_position('center')
+
+# Overlay the text clip on the first video clip
+final_video = CompositeVideoClip([clip, txt_clip])
+final_video.write_videofile("/home/rteam2/m15kh/video-subtitle/notebook/auto-subtitle/result.mp4")
